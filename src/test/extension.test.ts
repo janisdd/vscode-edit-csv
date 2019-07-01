b3570d5e84e7c159c777969f8b029891d1c67602
Janis Daniel Dähne <janisdd5566@googlemail.com>
2019-07-01 19:22:47 +0200

    - added tests for parition string that caused the issue #5

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -5,42 +5,70 @@
 
 // The module 'assert' provides assertion methods from node
 import * as assert from 'assert';
-import * as vscode from 'vscode'
-
-// You can import and use all API from the 'vscode' module
-// as well as import your extension to test it
-// import * as vscode from 'vscode';
-// import * as myExtension from '../extension';
-
-// Defines a Mocha test suite to group tests of similar kind together
-suite("Extension Tests", function () {
-
-    // Defines a Mocha unit test
-    test("Something 1", function() {
-        assert.equal(-1, [1, 2, 3].indexOf(5));
-        assert.equal(-1, [1, 2, 3].indexOf(0));
-    });
-
-    //see https://vscode.rocks/testing/
-    test('execute command', async function () {
-        
-        const newFile = vscode.Uri.parse('untitled:Untitled-2')
-
-        const document = await vscode.workspace.openTextDocument(newFile)
-        const textEditor = await vscode.window.showTextDocument(document)
-
-        await vscode.commands.executeCommand('edit-csv.edit')
-
-        // await sleep(1000)
-        assert.equal(textEditor, vscode.window.activeTextEditor)
+// import * as vscode from 'vscode'
+import { partitionString } from '../util';
 
 
-    })
-});
+// Defines a Mocha unit test
+// test("Something 1", function() {
+//     assert.equal(-1, [1, 2, 3].indexOf(5));
+//     assert.equal(-1, [1, 2, 3].indexOf(0));
+// });
 
+//see https://vscode.rocks/testing/
+// const newFile = vscode.Uri.parse('untitled:Untitled-2')
+// const document = await vscode.workspace.openTextDocument(newFile)
+// const textEditor = await vscode.window.showTextDocument(document)
+
+// await vscode.commands.executeCommand('edit-csv.edit')
+
+// // await sleep(1000)
+// assert.equal(textEditor, vscode.window.activeTextEditor)
 
 // function sleep(ms: number): Promise<void> {
 //     return new Promise(resolve => {
 //       setTimeout(resolve, ms)
 //     })
 //   }
+
+
+// Defines a Mocha test suite to group tests of similar kind together
+suite("partitionString working properly", function () {
+
+    test('partition size not fitting', async function () {
+        
+        const text = '0123456789'
+
+        const parts = partitionString(text, 3)
+
+        const margedText = parts.map(p => p.text).join('')
+
+        assert.equal(margedText, text)
+    })
+
+    test('partition size larger than text', async function () {
+        
+        const text = '0123456789'
+
+        const parts = partitionString(text, text.length+10)
+
+        const margedText = parts.map(p => p.text).join('')
+
+        assert.equal(margedText, text)
+    })
+
+    test('partition size perfect fit', async function () {
+        
+        const text = '0123456789'
+
+        const parts = partitionString(text, text.length)
+
+        const margedText = parts.map(p => p.text).join('')
+
+        assert.equal(margedText, text)
+    })
+
+});
+
+
+
