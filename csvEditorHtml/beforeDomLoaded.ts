3540b26633ab0cf2a72f124f1d4a439c5569ff97
Janis Daniel Dähne <janisdd5566@googlemail.com>
2020-10-18 20:07:54 +0200

    - fixed issue where in browser options bar was flickering   - now we also use css vars

--- csvEditorHtml/beforeDomLoaded.ts
+++ csvEditorHtml/beforeDomLoaded.ts
@@ -1,7 +1,4 @@
-//--- called before dom (body) is rendered (only use this in the vs code extension)
+//--- called before dom (body) is rendered
 
-if (typeof initialConfig !== 'undefined') { //only in vs code extension
-	document.documentElement.style
-		.setProperty('--extension-options-bar-display', initialConfig?.optionsBarAppearance === "collapsed" ? `none` : `block`)
-}
+document.documentElement.style.setProperty('--extension-options-bar-display', initialConfig?.optionsBarAppearance === "collapsed" ? `none` : `block`)
 
