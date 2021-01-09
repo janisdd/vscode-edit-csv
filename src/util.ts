b7144f050c9bbcb89986f8808b42a5504d06cdec
Janis Daniel Dähne <janisdd5566@googlemail.com>
2021-01-09 21:02:52 +0100

    - fixes issue #55 - adds support for different csv language ids (contributed by rainbow csv extension)

--- src/util.ts
+++ src/util.ts
@@ -36,7 +36,10 @@ export function isCsvFile(document: vscode.TextDocument) {
 	if (!document) return false
 
 	let lang = document.languageId.toLowerCase()
-	let possible = ['csv', 'csv (semicolon)', 'tsv', 'plaintext']
+	let possible = ['csv', 'tsv', 'plaintext',
+		//rainbow csv extension types, see https://github.com/mechatroner/vscode_rainbow_csv
+		'csv (semicolon)', 'csv (pipe)', 'csv (whitespace)', 'csv (tilde)', 'csv (caret)', 'csv (colon)', 'csv (double quote)', 'csv (equals)', 'csv (dot)', 'csv (hyphen)'
+	]
 	const _isCsvFile = possible.find(p => p === lang) && document.uri.scheme !== 'csv-edit'
 	return _isCsvFile
 }
